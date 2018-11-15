70ef37797280a55e1c0bd54b5b907948eab8ddc7
Darlene1985 <Darlene_Leslie@ymail.com>
2018-11-14 21:11:11 -0700

    added var

--- config/connection.js
+++ config/connection.js
@@ -2,24 +2,16 @@
 var mysql = require("mysql");
 
 if (process.env.JAWS_URL) {
-  connection = mysql.createConnection(process.env.JAWS_URL);
+  var connection = mysql.createConnection(process.env.JAWS_URL);
 } else {
   var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "root",
-    database: "burgers_db"
+    password: 'hacktheplanet',
+    database: 'todoagain_db'
   });
 };
-
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "root",
-//   database: "burgers_db"
-// });
 
 // Make connection.
 connection.connect(function (err) {
